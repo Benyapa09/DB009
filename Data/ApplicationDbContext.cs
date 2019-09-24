80088c7773e9a6eaf6e813dc069277fc0cac2393
benyapa09 <iceice-st@rmutsb.ac.th>
2019-09-24 16:38:16 +0700

    DBice

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataBase2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBase2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
